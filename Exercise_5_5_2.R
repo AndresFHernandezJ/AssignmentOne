2acf9953b006728b260c5d9173ee148c824fbc74
SantiagoAv22 <santiago.avilaq@ecci.edu.co>
2023-02-25 20:16:33 -0500

    Ya

--- Exercise_5_5_2.R
+++ Exercise_5_5_2.R
@@ -12,5 +12,5 @@ view
 flights_comparacionreal <- mutate(flights,arr_time=((arr_time %/% 100*60)+(arr_time %%100))
                                   %%1440,dep_time=((dep_time %/% 100*60)+(dep_time %%100))%%1440,difference=arr_time-dep_time,comparation=air_time-difference)
 
-head(flights_comparacion)
+head(flights_comparacionreal)
 View(flights_comparacionreal)
